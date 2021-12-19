b89b3876ada3a9ee64610fa15f052a874c10481c
adham618 <adhamtarek291@gmail.com>
2021-12-19 20:32:19 +0200

    adding recipeDetails

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,15 @@ const Recipes: NextPage = ({ recipes }) => {
       {recipes.map(recipe => (
         <RecipeCard key={recipe.sys.id} recipe={recipe} />
       ))}
+
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
+
     </div>
   )
 }
