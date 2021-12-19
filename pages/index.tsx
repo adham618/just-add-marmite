c6cfbb70386fcc82ad82ff2e21fcaefca967d21e
adham618 <adhamtarek291@gmail.com>
2021-12-19 21:48:01 +0200

    showing updating content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
 import { createClient } from 'contentful'
 import type { NextPage } from 'next'
 import RecipeCard from '../components/RecipeCard'
-const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-})
 
 export async function getStaticProps() {
 
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  })
 
   const res = await client.getEntries({ content_type: "recipe" })
 
   return {
     props: {
       recipes: res.items,
-      revalidate: 1
-    }
+    },
+    revalidate: 1
   }
 }
 
